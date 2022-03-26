a7982983514221bc60c7a017a77b3e3efc1db1b9
Martin <16228305+maartin0@users.noreply.github.com>
2022-03-26 18:28:41 +0000

    Flatten modules

---
+++ log_all_sensors.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import interrupt_client, MCP342X, wind_direction, HTU21D, bmp085, tgs2600, ds18b20_therm
+from database import Database
+
+HEADERS = [
+    "temperature0_c",
+    "temperature1_c",
+    "air_quality_percent",
+    "pressure_mb",
+    "humidity_percent",
+    "wind_direction_degrees",
+    "wind_speed_kmph",
+    "wind_gust_kmph",
+    "rainfall_mm",
+]
+
+print("Reading...")
+pressure = bmp085.BMP085()
+temp_probe = ds18b20_therm.DS18B20()
+air_qual = tgs2600.TGS2600(adc_channel = 0)
+humidity = HTU21D.HTU21D()
+wind_dir = wind_direction.wind_direction(adc_channel = 0, config_file="wind_direction.json")
+interrupts = interrupt_client.interrupt_client(port = 49501)
+
+wind_average = wind_dir.get_value(10) # ten seconds
+
+db = database.Database(HEADERS)
+
+print("Saving...")
+db.insert(
+    temp_probe.read_temp(),
+    humidity.read_temperature(),
+    air_qual.get_value(),
+    pressure.get_pressure(),
+    humidity.read_humidity(),
+    wind_average,
+    interrupts.get_wind(),
+    interrupts.get_wind_gust(),
+    interrupts.get_rain()
+    )
+print("done")
+
+interrupts.reset()
